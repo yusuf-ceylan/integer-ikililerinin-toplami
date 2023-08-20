483efc9e2d68a42531ccc695ae8a0d52ecac4ce7
Yusuf CEYLAN <yusufff91@gmail.com>
2023-08-21 00:46:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace integer_ikililerinin_toplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n tane integer sayı giriniz(sayıların arasına boşluk ekleyerek): ");
+            string input = Console.ReadLine();
+
+            string[] nums = input.Split(' ');
+            Dictionary<int, int> output = new Dictionary<int, int>();
+            int c = 0;
+
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                int num1 = int.Parse(nums[i]);
+                int num2 = int.Parse(nums[i + 1]);
+                int sum = num1 + num2;
+
+                if (num1 == num2)
+                {
+                    output.Add(c, sum * sum);
+                }
+                else
+                {
+                    output.Add(c, sum);
+                }
+                c++;
+            }
+
+            foreach (var item in output)
+            {
+                Console.Write(item.Value + " ");
+            }
+        }
+    }
+}
